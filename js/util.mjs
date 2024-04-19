3afcf849ec4a66f01131bce15b9ef7774811efb2
EricWehrly <eric@disruptiontheory.com>
2024-04-19 14:09:35 -0400

    save junk before deleting

--- js/util.mjs
+++ js/util.mjs
@@ -3,3 +3,19 @@ export async function fetchHtmlAsText(url) {
     const response = await fetch(url);
     return await response.text();
 }
+
+function getAttributesAsQueryParams(element) {
+
+    let result = '';
+    const blackList = [
+        'class'
+    ];
+
+    for(var attr of element.attributes) {
+        if(!blackList.includes(attr.name)) {
+            result += attr.name + '=' + attr.value + '&';
+        }
+    }
+
+    return result;
+}
