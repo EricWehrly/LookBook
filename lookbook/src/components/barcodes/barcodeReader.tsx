310ba2ed02bda79c9c3312c725616d66ce003be9
EricWehrly <eric@disruptiontheory.com>
2024-04-26 01:26:35 -0400

    (feat) [Barcode Scanner]: initial commit

---
+++ lookbook/src/components/barcodes/barcodeReader.tsx
@@ -0,0 +1,11 @@
+import { Component } from "react";
+import Quagga from '@ericblade/quagga2';
+
+class BarcodeReader extends Component {
+
+    constructor(props: Object) {
+        super(props);
+    }
+}
+
+export default BarcodeReader;
