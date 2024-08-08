a73244d701b7dfbab8f10e509a7c6c882a27281e
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-08-08 18:01:55 -0400

    define the lambda function

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -26,3 +26,48 @@ resource "aws_s3_object" "lambda_upc" {
 
   etag = filemd5(data.archive_file.lambda_upc.output_path)
 }
+
+resource "aws_lambda_function" "upc_lookup" {
+  function_name = "UPCLookup"
+
+  s3_bucket = aws_s3_bucket.lookbook_bucket.id
+  s3_key    = aws_s3_object.lambda_upc.key
+
+  runtime = "nodejs20.x"
+  handler = "lambda.handler"
+
+  source_code_hash = data.archive_file.lambda_upc.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+/* hopefully we don't need cloudwatch
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 30
+}
+*/
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
