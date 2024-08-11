49822015ba162e200ef82c8ec540c213099e3131
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-08-11 16:59:27 -0400

    need to drop the authorization check ...still need to figure out url path params

--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -76,7 +76,11 @@ resource "aws_api_gateway_method" "GetItem" {
   rest_api_id   = aws_api_gateway_rest_api.lookbook_api.id
   resource_id   = aws_api_gateway_resource.s3_item.id
   http_method   = "GET"
-  authorization = "AWS_IAM"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.key" = true
+  }
 }
 
 resource "aws_api_gateway_integration" "S3Integration" {
@@ -96,7 +100,11 @@ resource "aws_api_gateway_integration" "S3Integration" {
 
 /*
   request_parameters = {
+    name =  "key"
     "key" = "method.request.path.key"
+  }
+  request_templates = {                  # Not documented
+    "application/json" = "${file("api_gateway_body_mapping.template")}"
   }
   */
 }
