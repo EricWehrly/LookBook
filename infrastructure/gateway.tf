223a612ca3747eedac073ee38b6123cf10cfb601
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-08-11 15:08:14 -0400

    not working correctly yet

--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -6,13 +6,11 @@ resource "aws_api_gateway_rest_api" "lookbook_api" {
   name = "lookbook_api"
 }
 
-/* TODO: (Deployment) Stage
 resource "aws_api_gateway_deployment" "S3APIDeployment" {
   depends_on  = [aws_api_gateway_integration.S3Integration]
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   stage_name  = "lookbook_prod"
 }
-*/
 
 resource "aws_api_gateway_resource" "api_resource_upc" {
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
@@ -36,18 +34,21 @@ resource "aws_api_gateway_integration" "upc_lambda_integration" {
   uri                     = aws_lambda_function.upc_lookup.invoke_arn
 }
 
+/*
 resource "aws_api_gateway_resource" "Folder" {
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   parent_id   = aws_api_gateway_rest_api.lookbook_api.root_resource_id
   path_part   = "{folder}"
 }
+*/
 
-resource "aws_api_gateway_resource" "Item" {
+resource "aws_api_gateway_resource" "s3_item" {
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
-  parent_id   = aws_api_gateway_resource.Folder.id
-  path_part   = "{item}"
+  parent_id   = aws_api_gateway_rest_api.lookbook_api.root_resource_id
+  path_part   = "{key}"
 }
 
+/* we probably don't want this 
 resource "aws_api_gateway_method" "GetBuckets" {
   rest_api_id   = aws_api_gateway_rest_api.lookbook_api.id
   resource_id   = aws_api_gateway_rest_api.lookbook_api.root_resource_id
@@ -55,7 +56,7 @@ resource "aws_api_gateway_method" "GetBuckets" {
   authorization = "AWS_IAM"
 }
 
-resource "aws_api_gateway_integration" "S3Integration" {
+resource "aws_api_gateway_integration" "get_buckets_integration" {
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   resource_id = aws_api_gateway_rest_api.lookbook_api.root_resource_id
   http_method = aws_api_gateway_method.GetBuckets.http_method
@@ -69,7 +70,36 @@ resource "aws_api_gateway_integration" "S3Integration" {
   uri         = "arn:aws:apigateway:${var.aws_region}:s3:path//"
   credentials = aws_iam_role.s3_api_gateway_role.arn
 }
+*/
 
+resource "aws_api_gateway_method" "GetItem" {
+  rest_api_id   = aws_api_gateway_rest_api.lookbook_api.id
+  resource_id   = aws_api_gateway_resource.s3_item.id
+  http_method   = "GET"
+  authorization = "AWS_IAM"
+}
+
+resource "aws_api_gateway_integration" "S3Integration" {
+  rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
+  resource_id   = aws_api_gateway_resource.s3_item.id
+  http_method = aws_api_gateway_method.GetItem.http_method
+
+  # Included because of this issue: https://github.com/hashicorp/terraform/issues/10501
+  integration_http_method = "GET"
+
+  type = "HTTP_PROXY"
+
+  # See uri description: https://docs.aws.amazon.com/apigateway/api-reference/resource/integration/
+  uri = "http://lookbook.wehrly.com.s3-website-us-east-1.amazonaws.com/{key}"
+  # uri         = "arn:aws:apigateway:${var.aws_region}:s3:path//{folder}/{item}"
+  credentials = aws_iam_role.s3_api_gateway_role.arn
+
+/*
+  request_parameters = {
+    "key" = "method.request.path.key"
+  }
+  */
+}
 
 # Create S3 Full Access Policy
 resource "aws_iam_policy" "s3_policy" {
@@ -118,12 +148,11 @@ resource "aws_iam_role_policy_attachment" "s3_policy_attach" {
   policy_arn = aws_iam_policy.s3_policy.arn
 }
 
-
-
+/*
 resource "aws_api_gateway_method_response" "Status200" {
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   resource_id = aws_api_gateway_rest_api.lookbook_api.root_resource_id
-  http_method = aws_api_gateway_method.GetBuckets.http_method
+  http_method = aws_api_gateway_method.GetItem.http_method
   status_code = "200"
 
   response_parameters = {
@@ -138,29 +167,29 @@ resource "aws_api_gateway_method_response" "Status200" {
 }
 
 resource "aws_api_gateway_method_response" "Status400" {
-  depends_on = [aws_api_gateway_integration.S3Integration]
+  # depends_on = [aws_api_gateway_integration.S3Integration]
 
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   resource_id = aws_api_gateway_rest_api.lookbook_api.root_resource_id
-  http_method = aws_api_gateway_method.GetBuckets.http_method
+  http_method = aws_api_gateway_method.GetItem.http_method
   status_code = "400"
 }
 
 resource "aws_api_gateway_method_response" "Status500" {
-  depends_on = [aws_api_gateway_integration.S3Integration]
+  # depends_on = [aws_api_gateway_integration.S3Integration]
 
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   resource_id = aws_api_gateway_rest_api.lookbook_api.root_resource_id
-  http_method = aws_api_gateway_method.GetBuckets.http_method
+  http_method = aws_api_gateway_method.GetItem.http_method
   status_code = "500"
 }
 
 resource "aws_api_gateway_integration_response" "IntegrationResponse200" {
-  depends_on = [aws_api_gateway_integration.S3Integration]
+  # depends_on = [aws_api_gateway_integration.S3Integration]
 
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   resource_id = aws_api_gateway_rest_api.lookbook_api.root_resource_id
-  http_method = aws_api_gateway_method.GetBuckets.http_method
+  http_method = aws_api_gateway_method.GetItem.http_method
   status_code = aws_api_gateway_method_response.Status200.status_code
 
   response_parameters = {
@@ -171,34 +200,33 @@ resource "aws_api_gateway_integration_response" "IntegrationResponse200" {
 }
 
 resource "aws_api_gateway_integration_response" "IntegrationResponse400" {
-  depends_on = [aws_api_gateway_integration.S3Integration]
+  # depends_on = [aws_api_gateway_integration.S3Integration]
 
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   resource_id = aws_api_gateway_rest_api.lookbook_api.root_resource_id
-  http_method = aws_api_gateway_method.GetBuckets.http_method
+  http_method = aws_api_gateway_method.GetItem.http_method
   status_code = aws_api_gateway_method_response.Status400.status_code
 
   selection_pattern = "4\\d{2}"
 }
 
 resource "aws_api_gateway_integration_response" "IntegrationResponse500" {
-  depends_on = [aws_api_gateway_integration.S3Integration]
+  # depends_on = [aws_api_gateway_integration.S3Integration]
 
   rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
   resource_id = aws_api_gateway_rest_api.lookbook_api.root_resource_id
-  http_method = aws_api_gateway_method.GetBuckets.http_method
+  http_method = aws_api_gateway_method.GetItem.http_method
   status_code = aws_api_gateway_method_response.Status500.status_code
 
   selection_pattern = "5\\d{2}"
 }
+*/
 
-/*
 resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_api_gateway_rest_api.lambda.name}"
+  name = "/aws/api_gw/${aws_api_gateway_rest_api.lookbook_api.name}"
 
   retention_in_days = 30
 }
-*/
 
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
