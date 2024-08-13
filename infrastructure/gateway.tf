35ff10e8698ee43b847ce63cd25f4cf0cb134867
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-08-13 00:39:31 -0400

    add a root method to "redirect" to index that didn't work, but I think we'll need it, so we'll check it in anyway

--- infrastructure/gateway.tf
+++ infrastructure/gateway.tf
@@ -103,6 +103,29 @@ resource "aws_api_gateway_integration" "S3Integration" {
   }
 }
 
+resource "aws_api_gateway_method" "gateway_index_method" {
+  rest_api_id   = aws_api_gateway_rest_api.lookbook_api.id
+  resource_id   = aws_api_gateway_rest_api.lookbook_api.root_resource_id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "index_integration" {
+  rest_api_id = aws_api_gateway_rest_api.lookbook_api.id
+  resource_id   = aws_api_gateway_rest_api.lookbook_api.root_resource_id
+  http_method = aws_api_gateway_method.gateway_index_method.http_method
+
+  # Included because of this issue: https://github.com/hashicorp/terraform/issues/10501
+  integration_http_method = "GET"
+
+  type = "HTTP_PROXY"
+  passthrough_behavior    = "WHEN_NO_MATCH"
+
+  # See uri description: https://docs.aws.amazon.com/apigateway/api-reference/resource/integration/
+  uri = "http://lookbook.wehrly.com.s3-website-us-east-1.amazonaws.com/index.html"
+  credentials = aws_iam_role.s3_api_gateway_role.arn
+}
+
 # Create S3 Full Access Policy
 resource "aws_iam_policy" "s3_policy" {
   name        = "s3-policy"
