ef8bb461cec4c6137b4f3b27e2b875f3e0fcf5da
Eric Wehrly <1851833+EricWehrly@users.noreply.github.com>
2024-08-12 23:56:21 -0400

    get s3 site to serve objects

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -18,3 +18,19 @@ resource "aws_s3_bucket_public_access_block" "site_access" {
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
+
+resource "aws_s3_bucket_policy" "site_policy" {
+  bucket = aws_s3_bucket.lookbook_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.lookbook_bucket.arn}/*"
+      }
+    ]
+  })
+}
